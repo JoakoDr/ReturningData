b1a1f2d1f2b6de2a7539d95dcec6a67d6f525015
JoaquinDiazRamirez <joaco_diaz_ramirez@hotmail.com>
2019-01-16 11:05:56 +0100

    Floating button + tabbar

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,110 +1,111 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/FABBottomAppBarState.dart';
+import 'package:flutter_app/FABWithIcons.dart';
 import 'package:flutter_app/GridLayout.dart';
 import 'package:flutter_app/SecondScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/layout.dart';
 
+void main() => runApp(new MyApp());
 
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'BottomAppBar with FAB'),
+    );
+  }
+}
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
-
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Widget currentScreen;
-  SecondScreen _secondScreen = SecondScreen();
-  GridLayout _gridLayout = GridLayout();
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  String _lastSelected = 'TAB: 0';
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    currentScreen = _secondScreen;
-  }
-
-
-  void _incrementCounter() {
+  void _selectedTab(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _lastSelected = 'TAB: $index';
     });
   }
-  void decrementCounter(){
+
+  void _selectedFab(int index) {
     setState(() {
-      _counter = 0;
+      _lastSelected = 'FAB: $index';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              child: Container(
-                color: Colors.red,
-              )
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text("Home"),
-              onTap: ()
-                  {
-                    setState(() {
-                      currentScreen = _secondScreen;
-                      Navigator.pop(context);
-                    });
-
-                  },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text("Settings"),
-              onTap: ()
-                  {
-                    setState(() {
-                      currentScreen = _gridLayout;
-                      Navigator.pop(context);
-                    });
-
-                  },
-            )
-          ],
+      body: Center(
+        child: Text(
+          _lastSelected,
+          style: TextStyle(fontSize: 32.0),
         ),
       ),
-      body:currentScreen
+      bottomNavigationBar: FABBottomAppBar(
+        centerItemText: 'A',
+        color: Colors.grey,
+        selectedColor: Colors.red,
+        notchedShape: CircularNotchedRectangle(),
+        onTabSelected: _selectedTab,
+        items: [
+          FABBottomAppBarItem(iconData: Icons.menu, text: 'Item2'),
+          FABBottomAppBarItem(iconData: Icons.layers, text: 'Item2'),
+          FABBottomAppBarItem(iconData: Icons.dashboard, text: 'Item2'),
+          FABBottomAppBarItem(iconData: Icons.info, text: 'Item2'),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: _buildFab(
+          context), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _buildFab(BuildContext context) {
+    final icons = [ Icons.sms, Icons.mail, Icons.phone ];
+    return AnchoredOverlay(
+      showOverlay: true,
+      overlayBuilder: (context, offset) {
+        return CenterAbout(
+          position: Offset(offset.dx, offset.dy - icons.length * 35.0),
+          child: FabWithIcons(
+            icons: icons,
+            onIconTapped: _selectedFab,
+          ),
+        );
+      },
+      child: FloatingActionButton(
+        onPressed: () { },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        elevation: 2.0,
+      ),
     );
   }
 }
